ff64bc0ca5948e499d4acba5201a8a1e4de2cb19
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2019-10-30 09:21:22 +0100

    Show enabled/disabled plugin state  with check boxes in plugin manager

--- kadas/app/kadaspluginmanager.cpp
+++ kadas/app/kadaspluginmanager.cpp
@@ -302,7 +302,7 @@ void KadasPluginManager::setItemActivatable( QTreeWidgetItem *item )
 {
   if ( item )
   {
-    item->setText( 1, tr( "Activate" ) );
+    item->setCheckState( 1, Qt::Unchecked );
   }
 }
 
@@ -310,6 +310,6 @@ void KadasPluginManager::setItemDeactivatable( QTreeWidgetItem *item )
 {
   if ( item )
   {
-    item->setText( 1, tr( "Dectivate" ) );
+    item->setCheckState( 1, Qt::Checked );
   }
 }
